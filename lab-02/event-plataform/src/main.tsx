41838698dd5f0680042c2e7b67985e28436a3d66
Bruno Mariani <bruno0907@gmail.com>
2022-06-21 20:37:13 -0300

    chore: moved App file to src root and import fix. Added import of Event page to App

--- lab-02/event-plataform/src/main.tsx
+++ lab-02/event-plataform/src/main.tsx
@@ -2,7 +2,7 @@ import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { client } from './lib/apollo'
-import App from './pages/App'
+import App from './App'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
