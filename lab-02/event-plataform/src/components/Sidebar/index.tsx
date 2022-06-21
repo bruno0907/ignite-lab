d734fb8526c02093cbe43f88d722905f060f4e09
Bruno Mariani <bruno0907@gmail.com>
2022-06-21 20:33:38 -0300

    chore: added Sidebar bare component

---
+++ lab-02/event-plataform/src/components/Sidebar/index.tsx
@@ -0,0 +1,15 @@
+export const Sidebar = () => {
+  return (
+    <aside>
+      <h1>Lessons Schedule</h1>
+      <hr />
+      <nav>
+        <ul>
+          <li>Lesson 1</li>
+          <li>Lesson 2</li>
+          <li>Lesson 3</li>
+        </ul>
+      </nav>
+    </aside>
+  )
+}
