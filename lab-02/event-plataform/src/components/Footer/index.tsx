8f09ba5c444985ce7e125d916d1d21a6fc8cd181
Bruno Mariani <bruno0907@gmail.com>
2022-06-21 20:34:33 -0300

    chore: added Footer bare component

---
+++ lab-02/event-plataform/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { RocketseatLogo } from "../RocketseatLogo"
+
+export const Footer = () => {
+  return (
+    <footer>
+      <RocketseatLogo />
+      <span>Rocketseat - Todos os direitos reservados</span>
+      <a href="#">Políticas de privacidade</a>
+    </footer>
+  )
+}
