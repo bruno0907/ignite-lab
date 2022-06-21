3f6cdef74a49a729fb2bfe6f08a47e0e79f58fd5
Bruno Mariani <bruno0907@gmail.com>
2022-06-21 20:35:01 -0300

    chore: moved Header component to its folder

--- lab-02/event-plataform/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from "./Logo"
-
-export const Header = () => {
-  return (
-    <header className="w-screen p-4 sticky flex items-center justify-center bg-brand-base700">
-      <Logo />
-    </header>
-  )
-}
